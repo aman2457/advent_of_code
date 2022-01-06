d0bcacc93409bfefe0d1b5dee7785dc5dd280c8d
amankumar <amanverma2457@gmail.com>
2022-01-06 13:11:23 +0530

    Verify commit

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,16 +1,26 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        var prev = 0
+        var count = 0
+        for (i in 1 until input.size) {
+            println(i)
+            if (input[i] > input[prev]) {
+                count += 1
+            }
+            prev += 1
+        }
+        return count
     }
 
     fun part2(input: List<String>): Int {
+        println("Normal commit to check")
         return input.size
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
-
+    check(part2(testInput) == 8)
+    println(part1(testInput))
     val input = readInput("Day01")
     println(part1(input))
     println(part2(input))
